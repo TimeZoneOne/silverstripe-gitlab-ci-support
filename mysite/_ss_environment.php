f58bdba7bb6c45aee148fe750883348baa037ce9
Shawn Lin <shawn@timezoneone.com>
2017-05-11 10:43:27 +1200

    Database config

--- mysite/_ss_environment.php
+++ mysite/_ss_environment.php
@@ -1,8 +1,8 @@
 <?php
     define('SS_ENVIRONMENT_TYPE', 'dev');
-    define('SS_DATABASE_SERVER', 'localhost');
+    define('SS_DATABASE_SERVER', 'mysql');
     define('SS_DATABASE_USERNAME', 'root');
     define('SS_DATABASE_PASSWORD', 'root');
-    define('SS_DATABASE_NAME', 'test');
+    define('SS_DATABASE_NAME', 'ss_test');
     define('SS_DEFAULT_ADMIN_USERNAME', 'admin');
     define('SS_DEFAULT_ADMIN_PASSWORD', 'admin');
