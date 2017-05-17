4fdbc1cf2dce2324ac8fbedb1d6e50d5de3f2a13
Shawn Lin <shawn@timezoneone.com>
2017-05-17 17:33:56 +1200

    Add url mapping

--- mysite/_ss_environment.php
+++ mysite/_ss_environment.php
@@ -6,3 +6,6 @@
     define('SS_DATABASE_NAME', 'ss_test');
     define('SS_DEFAULT_ADMIN_USERNAME', 'admin');
     define('SS_DEFAULT_ADMIN_PASSWORD', 'admin');
+
+	global $_FILE_TO_URL_MAPPING;
+	$_FILE_TO_URL_MAPPING[dirname(__FILE__)] = 'http://ss_test.com';
